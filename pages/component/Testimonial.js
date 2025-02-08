386f06d70c4997f8446fe7c051dd04533aa10445
MD.AL-AMIN <66849857+mdalamin75@users.noreply.github.com>
2025-02-09 01:01:21 +0600

    Update Testimonial.js

--- pages/component/Testimonial.js
+++ pages/component/Testimonial.js
@@ -5,7 +5,7 @@ const testimonial = () => {
   return (
     <>
         <section id="testimonial" className="bg-orange-500 relative mt-20 pb-44">
-            <div className="container px-4 md:px-20 space-y-16">
+            <div className="container mx-auto px-4 md:px-20 space-y-16">
                 <div className="testimonial_head flex justify-center">
                     <h1 className="text-white text-center text-4xl font-bold bg-[#161c34] py-5 px-10 md:px-20 rounded-md">Testomonial</h1>
                 </div>
